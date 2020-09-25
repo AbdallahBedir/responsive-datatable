602475c793fb596947d38acc74cf487c49b70ce9
AbdallahBedir <abdallahahmed2025@gmail.com>
2020-09-25 17:57:55 +0200

    [Chore] Update homepage

---
+++ build/precache-manifest.d5d1ff4d98da1f1df75ecb28591f387b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9faac9537cbae3dc2c27204ab2ed399b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e2cc6c2de6b95f9f7e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9a9d1ffe8f0cda5fbe2c",
+    "url": "/static/js/2.285c9fd8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.285c9fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e2cc6c2de6b95f9f7e",
+    "url": "/static/js/main.9411efcf.chunk.js"
+  },
+  {
+    "revision": "4e1717ee02e16507c4e8",
+    "url": "/static/js/runtime-main.71da4540.js"
+  }
+]);
